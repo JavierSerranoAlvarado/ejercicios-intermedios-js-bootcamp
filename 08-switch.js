1c4a65943c20b445853cb990ba1a3c42cec66a02
Javier Serrano <javier.e.serrano.alvarado@gmail.com>
2021-09-22 21:56:08 -0500

    cambios menores

--- 08-switch.js
+++ 08-switch.js
@@ -4,7 +4,7 @@
     (Tipo if)
 */
 
-const edad = parseInt(prompt("Ingresa tu edad"));
+/* const edad = parseInt(prompt("Ingresa tu edad"));
 let mensaje = "";
 
 switch(edad){
@@ -17,5 +17,27 @@ switch(edad){
     case 70:
         mensaje = "Ya eres de la tercera edad";
         break;
+    default:
+        mensaje = "Fuera de rango de edad";
+        break;
 }
 
+document.write(`<p>${mensaje}</p>`); */
+
+let mensaje = "";
+
+const nombre = prompt("Ingresa tu nombre");
+
+switch(nombre) {
+    case 'carlos':
+        mensaje = 'eres administrador';
+        break;
+    case 'juan':
+        mensaje = 'Eres usuario';
+        break;
+    default:
+        mensaje = ' no tienes permiso para usar el sistema';
+        break;
+}
+
+document.write(`<p>${mensaje}</p>`);
